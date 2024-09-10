4e641fe053850b8c7ec6cf460433fa6c1722d8d5
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-09-10 16:30:48 +0000

    update folder name add new folder

---
+++ jsClassWork/classWork.js
@@ -0,0 +1,19 @@
+// Operators
+// uniary (one operand); x++
+// binary (two operands); x + y
+// ternary (three operands); x ? y : z  (condition ? exprIfTrue : exprIfFalse
+//    this is the conditional operand
+
+
+
+
+
+// Loops
+
+// for loop
+// for(initialization; CSSConditionRule; increment/decrement) {}
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+
