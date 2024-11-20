6afb6e24166ef66aa7d571cdb8118e8c36e4135e
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-11-20 00:54:11 +0000

    event manager set up

---
+++ event-manager/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import EventList from './components/EventList';
+import EventForm from './components/EventForm';
+
+const App = () => {
+
+    let list = EventList(props);
+    
+    let div;
+    return div
+};
+
+export default App;
---
+++ event-manager/src/components/EventDetails.js
@@ -0,0 +1,10 @@
+// EventDetails: Displays detailed information about a single event.
+
+
+import React from 'react';
+
+function EventList(events, onDelete){
+  return list
+  }
+
+export default EventList;
