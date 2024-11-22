87b4eeacc35e62acb08451d000a53fd8bcacf0e6
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-11-22 14:45:47 +0000

    delete

--- event-manager/src/components/EventDetails.jsx
+++
@@ -1,9 +0,0 @@
-// EventDetails: Displays detailed information about a single event.
-
-import React from "react";
-
-const EventDetails = () => {
-  return <div>EventDetails</div>;
-};
-
-export default EventDetails;
